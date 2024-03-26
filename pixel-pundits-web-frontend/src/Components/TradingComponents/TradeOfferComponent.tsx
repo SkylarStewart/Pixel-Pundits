ad890a3526b6811af6e51fb57b81a9ecd69864ba
Samuel Bricker <sbricker216@gmail.com>
2024-03-26 15:48:58 -0400

    Attempt at card info

--- pixel-pundits-web-frontend/src/Components/TradingComponents/TradeOfferComponent.tsx
+++ pixel-pundits-web-frontend/src/Components/TradingComponents/TradeOfferComponent.tsx
@@ -1,11 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button, Row, Col, Container } from 'react-bootstrap';
-import { Trade } from '../../TypeSheet';
+import { Trade, CardObj } from '../../TypeSheet';
 import { UserContext } from '../../contexts/user.context';
 import { updateAcceptStatus, deleteTrade, confirmTradeAsMaker } from '../TradeDatabaseControl';
+import CardDisplayRow from '../CardDisplayRow';
+import { getDBCard } from '../CardDatabaseControl';
 
 export default function TradeOfferComponent({ trade }: { trade: Trade }) {
     const { user } = useContext(UserContext);
+    
+    function convertDBtoCardOBJ(card: Promise<any>){
+        card.then((result) => {
+            console.log(result.name); // Access the name property once the promise is resolved
+            return{
+                name: result.name,
+                set: result.set,
+                price: result.price,
+                imageURL: result.imageURL,
+                print: result.print,
+                setCode: result.setCode,
+                cardId: result.number
+            };
+        }).catch((error) => {
+            //alert('Promise error ' + error);
+            console.log("Promise error: " + error);
+        });
+
+        return {
+            name: "",
+            set: "",
+            price: 0,
+            imageURL: "",
+            print: "",
+            setCode: "",
+            cardId: 0
+        };
+    }
 
     return (
         <Container>
@@ -19,7 +49,7 @@ export default function TradeOfferComponent({ trade }: { trade: Trade }) {
                     <Row>
                         {trade.tradeMakerCards.map((card, index) => (
                             <Container key={index}>
-                                <p>{card}</p>
+                                <CardDisplayRow card={convertDBtoCardOBJ(getDBCard(user, card))}/>
                             </Container>
                         ))}
                     </Row>
